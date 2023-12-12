729a0220f053120cf9f516a2c278a773b1338180
Heart_Crasher <140791604+Anmoral@users.noreply.github.com>
2023-12-13 00:04:10 +0300

    Add files via upload

---
+++ simple-bot.py
@@ -0,0 +1,21 @@
+import telebot
+from telebot import types
+
+token = "6755112066:AAFrLEdlmoiGC1ZmeMjKAQwzQATJ0qtkB5k"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею отвечать на запросы')
+
+@bot.message_handler(content_types=['хочу'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда тебе сюда - https://mtuci.ru/')
